557e404ba6a1c3c32dcc857df175446d2b6b62ae
Jon Kalfayan <jonk1993@gmail.com>
2015-09-16 07:35:00 -0700

    added test function

--- first_steps.py
+++ first_steps.py
@@ -3,24 +3,42 @@ import numpy as np
 import os
 import csv
 
-kaggleFolder = "/Users/jonkalfayan/Documents/GitRepos/Kaggle/"
+kaggleFolder = "/Users/Jonk/Documents/Developer/GitRepos/Kaggle/"
 train_data = kaggleFolder + "trainResized/"
 train_labels = kaggleFolder + "trainLabels.csv"
+test_data = kaggleFolder + "testResized/"
 
 x_train = []
 y_train = []
+
+x_test = []
 
 for filename in os.listdir(train_data):
 	img = train_data + filename
 	img_arr = ndimage.imread(img, True)
 	x_train.append(img_arr.ravel())
 
+for filename in os.listdir(test_data):
+    img = test_data + filename
+    img_arr = ndimage.imread(img, True)
+    x_test.append(img_arr.ravel())
+
+x_test = np.matrix(x_test)
 x_train = np.matrix(x_train)
 
 with open(train_labels, 'rb') as f:
     reader = csv.reader(f)
     y_train = list(reader)
 
-print y_train[2]
+predictions = []
+k = 1
+
+print x_test[1:2]
+
+def kNN():
+    pass
+    
+
+
 
 
