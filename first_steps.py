ff3d699de62faf904e26b49cd0099aada690c1b1
Jon Kalfayan <jkalfayan@apple.com>
2015-09-16 18:20:04 -0700

    added code for knn

--- first_steps.py
+++ first_steps.py
@@ -21,6 +21,9 @@ with open(train_labels, 'rb') as f:
     reader = csv.reader(f)
     y_train = list(reader)
 
+y_train.pop(0)
+
+print y_train[0]
 
 
 
@@ -34,10 +37,30 @@ with open(train_labels, 'rb') as f:
 
 
 
+def findMaxLabel(neighbors):
+	helper_dict = dict()
+	for neighbor in neighbors:
+		if neighbor in helper_dict:
+			helper_dict[neighbor] += 1
+		else:
+			helper_dict[neighbor] = 1
+	return max(helper_dict.iterkeys(), key=(lambda key: helper_dict[key]))
 
 
+def findNearestNeighborsLabel(img, k):
+	neighborDists = []
+	for train_img in x_train:
+		dist = squaredDist(img, train_img)
+		neighborDists.append(dist)
+	neighbors = []
+	for j in range(k):
+		leastDist = neighborDists.index(min(neighborDists))
+		neighbors.append(y_train[leastDist])
+		neighborDists.remove(min(neighborsDists))
+	return findMaxLabel(neighbors)
 
-
+			
+	
 
 
 def squaredDist(p1, p2):
